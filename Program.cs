70909186e30a39dd551bb6374f0fbfbf4b0aa844
Mazen <mazenbimboo@github.com>
2026-07-18 00:05:50 +0300

    C# basics

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program{
+static void Main(string[] args)
+{
+    string name = Console.ReadLine();
+    int age = 21;
+    for(int i =0; i<30;i++)
+    {
+        if (i==21)
+        {
+        Console.WriteLine($"His name is {name} and age is {age}");
+        }
+    }
+    List<int> nums = new List<int>();
+    nums.Add(1);
+
+    
+}
+}

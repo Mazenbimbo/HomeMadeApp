6959ca634b75cfa4aa18c24b41f39c9f53438bd4
Mazen <mazenbimboo@github.com>
2026-07-18 18:56:27 +0300

    OOP

--- Program.cs
+++ Program.cs
@@ -1,19 +1,48 @@
 ﻿using System;
+using System.Security.Cryptography;
 class Program{
+class Student{
+        public string name {get;set;}
+
+        public List<string> courses = new List<string>();
+        public int age{get;set;}
+}
 static void Main(string[] args)
 {
-    string name = Console.ReadLine();
-    int age = 21;
-    for(int i =0; i<30;i++)
-    {
-        if (i==21)
-        {
-        Console.WriteLine($"His name is {name} and age is {age}");
-        }
-    }
-    List<int> nums = new List<int>();
-    nums.Add(1);
+    List<Student> students = new List<Student>();
+    
+    Student s1 = new Student();
+    s1.name = "Hala";
+    s1.courses.Add("Math");
+    s1.courses.Add("Reading");
+    s1.courses.Add("Science");
+    s1.courses.Add("Phonix");
+    s1.courses.Add("Health");
+    s1.courses.Add("Writing");
+    s1.age = 8;
 
+    Student s2 = new Student();
+    s2.name = "Farouq";
+    s2.courses.Add("Math");
+    s2.courses.Add("PE");
+    s2.courses.Add("Science");
+    s2.age = 9;
+
+    Student s3 = new Student();
+    s3.name = "Haya";
+    s3.courses.Add("Math");
+    s3.courses.Add("English");
+    s3.courses.Add("Art");
+    s3.courses.Add("Math");
+    s3.age = 11;
+
+    students.Add(s1);
+    students.Add(s2);
+    students.Add(s3);
+    for(int i =0; i < students.Count; i++)
+        {
+            Console.WriteLine($"My name is {students[i].name}, my age is {students[i].age} and my cources is {string.Join(", ",students[i].courses)}");
+        }
     
 }
 }
--- obj/Debug/net8.0/csharp_projects.AssemblyInfo.cs
+++ obj/Debug/net8.0/csharp_projects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp_projects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70909186e30a39dd551bb6374f0fbfbf4b0aa844")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp_projects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp_projects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
